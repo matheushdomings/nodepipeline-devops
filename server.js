6eda3ace05ee525abd4f66de1e0dc36e7c3db51d
matheushdomings <mh.domingos@outlook.com>
2025-06-05 22:24:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const os = require('os')
 const hostname = os.hostname()
 
 const router = express.Router();
-const port = 3000;
+const port = process.env.PORT || 3000;
  
 // Configurar métricas padrão (CPU, Heap, Event Loop)
 client.collectDefaultMetrics({ register });
